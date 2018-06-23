a08435412e2013112820a5ab5152f17c56298dc0
Aktarul Islam <aktarul857@gmail.com>
2018-06-24 05:24:17 +0600

    canvassing report done

--- front-end/src/app/components/canvassing-report/canvassing-report.component.ts
+++ front-end/src/app/components/canvassing-report/canvassing-report.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../../services/auth.service";
 import {Router} from "@angular/router";
+import {CandidateService} from "../../services/candidate.service";
+import {Candidate} from "../../models/candidate";
 
 @Component({
   selector: 'app-canvassing-report',
@@ -9,12 +11,29 @@ import {Router} from "@angular/router";
 })
 export class CanvassingReportComponent implements OnInit {
 
+  candidates: Array<Candidate> = new Array<Candidate>();
+  newCandidates: Array<Candidate> = new Array<Candidate>();
+
+  tempCan = new Candidate();
+
+  searchKey: any;
+
   constructor(
     public authService: AuthService,
-    private router: Router
+    private router: Router,
+    private candidateService: CandidateService
   ) { }
 
   ngOnInit() {
+    this.candidateService.getCandidate()
+      .subscribe(res =>{
+        this.candidates = res.data;
+        console.log(this.candidates);
+
+        this.sort();
+      });
+
+
   }
 
   logOut() {
@@ -29,6 +48,96 @@ export class CanvassingReportComponent implements OnInit {
 
   loginPage() {
     this.router.navigate(['login']);
+  }
+
+  sort() {
+    var length = this.candidates.length;
+
+    for(var i = 0; i < length - 1 ; i++ ){
+      for(var j = 0; j < length - i - 1 ; j++) {
+        // console.log(i + " " + j + " " + this.candidates2[i].total_vote + " " + this.candidates2[j].total_vote);
+        if(this.candidates[j].total_vote < this.candidates[j+1].total_vote){
+          this.tempCan = this.candidates[j];
+          this.candidates[j] = this.candidates[j+1];
+          this.candidates[j+1] = this.tempCan;
+        }
+      }
+    }
+  }
+
+  president() {
+    this.searchKey = "President";
+    console.log('At search = '+ this.searchKey);
+    this.candidateService.getSearchCandidate(this.searchKey)
+      .subscribe(res => {
+        console.log(res.data);
+        this.candidates = res.data;
+        this.newCandidates = res.data;
+        this.sort();
+      });
+  }
+
+  vice_president() {
+    this.searchKey = "Vice President";
+    console.log('At search = '+ this.searchKey);
+    this.candidateService.getSearchCandidate(this.searchKey)
+      .subscribe(res => {
+        console.log(res.data);
+        this.candidates = res.data;
+        this.newCandidates = res.data;
+        this.sort();
+      });
+  }
+
+
+  members() {
+    this.searchKey = "Member";
+    console.log('At search = '+ this.searchKey);
+    this.candidateService.getSearchCandidate(this.searchKey)
+      .subscribe(res => {
+        console.log(res.data);
+        this.candidates = res.data;
+        this.newCandidates = res.data;
+        this.sort();
+      });
+  }
+
+
+  general_secretary() {
+    this.searchKey = "General Secretary";
+    console.log('At search = '+ this.searchKey);
+    this.candidateService.getSearchCandidate(this.searchKey)
+      .subscribe(res => {
+        console.log(res.data);
+        this.candidates = res.data;
+        this.newCandidates = res.data;
+        this.sort();
+      });
+  }
+
+
+  joint_secretary() {
+    this.searchKey = "Joint Secretary";
+    console.log('At search = '+ this.searchKey);
+    this.candidateService.getSearchCandidate(this.searchKey)
+      .subscribe(res => {
+        console.log(res.data);
+        this.candidates = res.data;
+        this.newCandidates = res.data;
+        this.sort();
+      });
+  }
+
+  treasurer() {
+    this.searchKey = "Treasurer";
+    console.log('At search = '+ this.searchKey);
+    this.candidateService.getSearchCandidate(this.searchKey)
+      .subscribe(res => {
+        console.log(res.data);
+        this.candidates = res.data;
+        this.newCandidates = res.data;
+        this.sort();
+      });
   }
 
 }
