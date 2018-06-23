96a2e4f0f304f8047f1129269e8bc0c224753b5e
Aktarul Islam <aktarul857@gmail.com>
2018-06-24 03:32:35 +0600

    home message updated

--- front-end/src/app/components/home/home.component.ts
+++ front-end/src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {AuthService} from "../../services/auth.service";
+import {VoterService} from "../../services/voter.service";
+import {Voter} from "../../models/voter";
 
 @Component({
   selector: 'app-home',
@@ -9,12 +11,25 @@ import {AuthService} from "../../services/auth.service";
 })
 export class HomeComponent implements OnInit {
 
+  id: any;
+
+  voter = new Voter();
+
   constructor(
     private router: Router,
-    public authService: AuthService
+    public authService: AuthService,
+    protected voterService: VoterService
   ) { }
 
   ngOnInit() {
+    if(true){
+      this.id = localStorage.getItem('loginId');
+      this.voterService.getSingleVoter(this.id)
+        .subscribe(res=>{
+          this.voter = res.data;
+          console.log(this.voter.status);
+        });
+    }
   }
 
   logOut() {
