80277b1fb84ff757589292677d72725d8386e585
Aktarul Islam <aktarul857@gmail.com>
2018-06-24 03:40:43 +0600

    inner navbar added to canvass

--- front-end/src/app/components/canvassing-report/canvassing-report.component.ts
+++ front-end/src/app/components/canvassing-report/canvassing-report.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-canvassing-report',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CanvassingReportComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  logOut() {
+
+    this.authService.logout();
+
+    // this.flashMessage.show('Your are successfully logged out!', {cssClass: 'alert-success'});
+
+    this.router.navigate(['/login']);
+    return false;
+  }
+
+  loginPage() {
+    this.router.navigate(['login']);
   }
 
 }
