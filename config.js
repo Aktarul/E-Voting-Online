e491bec3b658c8d98e84b5fc5422963b363d4b40
Aktarul Islam <aktarul857@gmail.com>
2018-07-06 11:26:04 +0600

    database updated

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     'tokenexp': 3600,
     'secret': 'my_secret_key',
     // 'database':'mongodb://localhost:27017/E-voting',
-    'database':'mongodb://E-vote:evote1234@ds227821@ds129541.mlab.com:29541/evoting'
+    'database':'mongodb://evote:evote1234@ds129541.mlab.com:29541/evoting'
 };
 
 
